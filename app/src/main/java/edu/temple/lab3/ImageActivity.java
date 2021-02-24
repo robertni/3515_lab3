93a5fd7b9ea9e6950750f7afa5ced42ad7ba70a4
Robert Ni <robertni@outlook.fr>
2021-02-24 11:16:16 -0500

    added array and spinner listener

---
+++ app/src/main/java/edu/temple/lab3/ImageActivity.java
@@ -0,0 +1,52 @@
+package edu.temple.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ImageActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // reference views
+        spinner = findViewById(R.id.spinner);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        // create array list of dogs
+        ArrayList dogArray = new ArrayList<String>();
+        dogArray.add("dog1");
+        dogArray.add("dog2");
+        dogArray.add("dog3");
+        dogArray.add("dog4");
+
+        // TODO images for dogs
+        // TODO custom adapter
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
